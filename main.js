0b47be04cdfb278fe5e2d75b2382fad09d7d007e
samandarsxdev <93664571+samandarsxdev@users.noreply.github.com>
2021-11-24 13:51:40 -0500

    add

--- main.js
+++ main.js
@@ -1,4 +1,141 @@
-function fromToAdd(){
+// function fromToAdd(){
+
+// }
+// console.log(fromToAdd());
+
+// function salom(name){
+//     if(!name){
+//      name = " mehmon"
+//     }
+//     return `Assalomu alaykum ${name}`
+// }
+// console.log(salom());
+
+// let theNumber = Number(prompt("Pick a number"));
+// if (!Number.isNaN(theNumber)) {
+// console.log("Your number is the square root of " +
+// theNumber * theNumber);
+// }
+
+// let Num  = number (prompt("son kiriting"));
+// if (!number.isNaN(Num)){
+//     console.log("sizning son kvadratingiz"+Num*Num);
+// } else{
+//     console.log("bu raqam emas");
+// }
+
+// let theNumber = Number(prompt("Pick a number"));
+// if (!Number.isNaN(theNumber)) {
+// console.log("Your number is the square root of " +
+// theNumber * theNumber);
+// } else {
+// console.log("Hey. Why didn't you give me a number?");
+// }
+
+// for(var i=100;i>0;i=i-1){
+//     console.log(i);
+// }
+// var massiv = []
+// var res = prompt("1")
+// massiv=push(res)
+// var res = prompt("2")
+// massiv=push(res)
+// var res = prompt("3")
+// massiv=push(res)
+
+// console.log(massiv);
+
+// var firs = prompt("")
+// // console.log(firs.split(,));
+// var arry = [20, 2, 3, 90];
+
+// // var res = arry.length;
+
+// for(var i = 0 ; i<arry.length ; i++)
+
+// {
+ 
+//   console.log(i);
+// }
+
+// console.log(arry);
+
+// function reverse(arry){
+//   var res = []
+//   for(var i=0; i<arry.length; i++)
+//   {
+//     console.log(i, arry[i]);
+//   }
+// }
+// reverse(1,2,3)
+
+// function fizzbuz(start , end){
+//   var res = []
+//   for(var i = start; i<=end; i++){
+//     if(i%15==0){
+//       res.push("fizzbuz")
+//     }else if(i%3==0){
+//       res.push("fizz")
+//     }else if(i%5==0){
+//       res.push("buz")
+//     }else{
+//       res.push(i)
+//     }
+//   }
+//   return res;
+// }
+// console.log(fizzbuz(1,90));
+
+// var arry = [1,2,3,4,5,6]
+// for(var item of arry){
+//   console.log(item);
+// }
+
+// var arry = []
+// function addUser(name){
+//   var obj={
+//   }
+//   return addUser
+// }
+// console.log(addUser("assalomu alaykum"));
+
+// function aniqlash(start ,end){
+//   var arry =  [];
+//   for(var i=start;i<=end;i++){
+//     if(i%3==0){
+//       arry.push(i)
+//     // }else{
+//     //     console.log("hechnima emas");
+//     // }
+//     }
+//   }
+// return arry
   
+// }
+// console.log(aniqlash(3,100));
+
+
+// function bezash(number, symbol){
+//   for(var i = number-1;i>0;i--){
+//     console.log(symbol.repeat(i));
+//    }
+//  for(var i = 2; i<=number-1;i++){
+//    console.log(symbol.repeat(i));
+//  }
+
+// }
+
+// bezash(4,"#")
+function get(){
+  var filter=[1,2,3,4,0,true,false,undefined,null];
+  for(var i = filter[0];i<=filter[8];i++){
+    if(!i==true){
+      filter.push(i);
+    }
+
+  }
+  return filter
 }
-console.log(fromToAdd());
+console.log(get());
+
+
